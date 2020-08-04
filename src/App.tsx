6345d1b8e8ae7251f6f9c05f334a46d4bf7e77e5
LaneriTiago <tiago.laneri93@gmail.com>
2020-08-04 00:34:21 -0300

    ajustes na landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import './assets/styles/global.css';
-import logoImg from './assets/images/logo.svg';
 
-function App() {
-  return (
-    <div id="page-landing">
-      <div id="page-landing-content" className="container">
-        <div id="logo-container">
-          <img src={logoImg} alt=""/>
-        </div>
-      </div>
-    </div>
-  );
+import Landing from './pages/landing';
+
+function App () {
+       return (
+       <Landing />
+        ) 
 }
 
-
-
 export default App;
+
+
